dcda2434ecf88ff714aa83faa941cb9362cda218
sisoubou <serena.fadda8@gmail.com>
2026-04-09 09:37:43 +0200

    initialisation du back-end MySQL

--- Database.py
+++ Database.py
@@ -16,3 +16,101 @@ def get_categories():
     cursor.close()
     conn.close()
     return categories
+
+def get_type_prestas_by_category(category_id):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM type_presta WHERE id_categorie = %s", (category_id,))
+    type_prestas = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return type_prestas
+
+def get_type_presta_details(presta_id):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM type_presta WHERE id_type_presta = %s", (presta_id,))
+    type_presta = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return type_presta
+
+def user_exists(email):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("SELECT id_user FROM user WHERE email = %s", (email,))
+    result = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return result is not None
+
+def create_users(nom, prenom, email, mdp):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO user (nom, prenom, email, mdp) VALUES (%s, %s, %s, %s)",
+                   (nom, prenom, email, mdp))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def authenticate_users(email, mdp):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user WHERE email = %s AND mdp = %s", (email, mdp))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return user
+
+def get_users_details(user_id):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM user WHERE id_user = %s", (user_id,))
+    user = cursor.fetchone()
+    cursor.close()
+    conn.close()
+    return user
+
+def update_user_details(user_id, nom, prenom, email):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE user SET nom = %s, prenom = %s, email = %s WHERE id_user = %s",
+                   (nom, prenom, email, user_id))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def create_prestation(id_user, id_type_presta, debut, fin, adresse):
+    conn = get_connection()
+    cursor = conn.cursor()
+    
+    cursor.execute("INSERT INTO prestation (id_user, debut_contrat, fin_contrat, adresse, status) VALUES (%s, %s, %s, %s, %s)",
+                   (id_user, debut, fin, adresse, "En attente"))
+    
+    id_presta = cursor.lastrowid
+    
+    cursor.execute("INSERT INTO relation_type_presta (id_presta, id_type_presta) VALUES (%s, %s)",
+                   (id_presta, id_type_presta))
+    
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+def get_notifs(user_id):
+    conn = get_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("SELECT * FROM notif WHERE id_user = %s", (user_id,))
+    notifs = cursor.fetchall()
+    cursor.close()
+    conn.close()
+    return notifs
+
+def mark_notif_as_read(notif_id):
+    conn = get_connection()
+    cursor = conn.cursor()
+    cursor.execute("UPDATE notif SET a_lu = 1 WHERE id_notif = %s", (notif_id,))
+    conn.commit()
+    cursor.close()
+    conn.close()
+
+
